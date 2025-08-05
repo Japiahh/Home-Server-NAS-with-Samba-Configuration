1cd5a39844ab1f7e1c5f4a51069a805e0f230bd6
Japiahh <mochamadrizkyka@gmail.com>
2025-08-05 13:35:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-#Update system
-sudo apt update && sudo apt upgrade
-
 #Install Samba
+sudo apt update && sudo apt upgrade
 sudo apt install samba
 
+#Create directory
+mkdir /home/#users/#folders
+
+#access to all users
+chmod 777 /home/#users/#folders
+
+#file access /etc/samba/smb.conf
+sudo nano /etc/samba/smb.conf
